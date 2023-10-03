39a9606688e9bfe7f9730086187e3f068b32d9a0
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-10-03 11:31:16 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio7-Ejercicio2-consola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7_Ejercicio2_consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Divisor de Omar Orlandini " + num + " : " + i);
+                    num++;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
